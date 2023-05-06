15b5f856cb86e049bc3a465fa295c8b8fffe6124
LuaRitz <lukinhasfht@gmail.com>
2023-05-06 09:50:06 -0300

    promodelo
    aaa

--- Modelo/ProModelo.cs
+++ Modelo/ProModelo.cs
@@ -8,5 +8,46 @@ namespace Modelo
 {
     internal class ProModelo
     {
+        private int codigoproduto;
+        private string nomeproduto;
+        private int stock;
+        private float preco;
+        private string imagem;
+
+
+        public ProModelo()
+        {
+            codigoproduto = 0;
+            nomeproduto = "";
+            stock = 0;
+            preco = 0;
+            imagem = "//Fotos/default.png";
+        }
+
+        public int codProduto
+        {
+            get { return codigoproduto; }
+            set { codigoproduto = value; }
+        }
+        public string NomePro
+        {
+            get { return nomeproduto; }
+            set { nomeproduto = value; }
+        }
+        public int Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+        public float Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+        public string Imagem
+        {
+            get { return imagem; }
+            set { imagem = value; }
+        }
     }
 }
