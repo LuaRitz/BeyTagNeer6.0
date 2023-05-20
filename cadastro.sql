a9138946f514e39696da5c9fef970914886968cb
LuaRitz <lukinhasfht@gmail.com>
2023-05-20 15:23:18 -0300

    Update cadastro.sql

--- cadastro.sql
+++ cadastro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 06-Maio-2023 às 14:46
+-- Generation Time: 20-Maio-2023 às 20:22
 -- Versão do servidor: 10.1.32-MariaDB
 -- PHP Version: 7.0.30
 
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `cadastro`
 --
-CREATE DATABASE IF NOT EXISTS `cadastro` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `cadastro` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `cadastro`;
 
 -- --------------------------------------------------------
@@ -48,10 +48,31 @@ DROP TABLE IF EXISTS `produtos`;
 CREATE TABLE `produtos` (
   `produto` int(2) NOT NULL,
   `nomeproduto` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `imagem` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagem` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Stock` int(11) NOT NULL,
   `preco` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`produto`, `nomeproduto`, `imagem`, `Stock`, `preco`) VALUES
+(1, 'Alatreon', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Alatreon.jpg', 25, '499.00'),
+(2, 'Astalos', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Astalos.png', 25, '299.00'),
+(3, 'Brachydios', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Brachydios.jpg', 73, '435.00'),
+(4, 'Diablos', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Diablos.jpg', 32, '326.00'),
+(5, 'Fatalis', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Fatalis.jpg', 19, '678.00'),
+(6, 'Glavenus', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Glavenus.jpg', 65, '234.90'),
+(7, 'Lagiacrus', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Lagiacrus.jpg', 45, '294.90'),
+(8, 'Malfiestio', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Malfestio.jpg', 53, '394.90'),
+(9, 'Malzeno', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Malzeno.jpg', 42, '479.00'),
+(10, 'Mizutsune', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Mizutsune.jpg', 69, '690.00'),
+(11, 'Nargacuga', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Nargacuga.jpg', 44, '240.00'),
+(12, 'Odagaron', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Odagaron.jpg', 32, '420.00'),
+(13, 'SafiJiiva', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\SafiJiiva.jpg', 63, '370.00'),
+(14, 'Tigrex', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Tigrex.jpg', 24, '470.00'),
+(15, 'Valstrax', 'C:\\Users\\Aluno\\Documents\\GitHub\\BeyTagNeer6.0\\Fotos\\Action_figure\\Valstrax.jpg', 41, '469.00');
 
 -- --------------------------------------------------------
 
@@ -90,6 +111,12 @@ ALTER TABLE `carrinhocompras`
   ADD PRIMARY KEY (`carrinho`);
 
 --
+-- Indexes for table `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`produto`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -104,6 +131,12 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `carrinhocompras`
   MODIFY `carrinho` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `produto` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `usuario`
