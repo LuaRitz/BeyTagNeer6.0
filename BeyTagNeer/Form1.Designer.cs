9c02614dd0bd386096769fb13a9d69b8a64956d6
LuaRitz <lukinhasfht@gmail.com>
2023-05-20 09:58:25 -0300

    tentando listar produtos

---
+++ BeyTagNeer/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BeyTagNeer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.preco = new System.Windows.Forms.Label();
+            this.comprar = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comprar);
+            this.panel1.Controls.Add(this.preco);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(544, 592);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(257, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 179);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // preco
+            // 
+            this.preco.AutoSize = true;
+            this.preco.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.preco.ForeColor = System.Drawing.Color.Teal;
+            this.preco.Location = new System.Drawing.Point(149, 210);
+            this.preco.Name = "preco";
+            this.preco.Size = new System.Drawing.Size(51, 23);
+            this.preco.TabIndex = 2;
+            this.preco.Text = "preco";
+            // 
+            // comprar
+            // 
+            this.comprar.AutoSize = true;
+            this.comprar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comprar.Location = new System.Drawing.Point(114, 233);
+            this.comprar.Name = "comprar";
+            this.comprar.Size = new System.Drawing.Size(74, 23);
+            this.comprar.TabIndex = 3;
+            this.comprar.TabStop = true;
+            this.comprar.Text = "Comprar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 712);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel comprar;
+        private System.Windows.Forms.Label preco;
+    }
+}
---
+++ BeyTagNeer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Control;
+using Modelo;
+
+namespace BeyTagNeer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UsuModelo usM = new UsuModelo();
+            usucontroller usC = new usucontroller();
+            ProModelo usP = new ProModelo();
+
+            for (int i = 1; i < 2; i++)
+            {
+                usP.codProduto = 1;
+            }
+            usP.codProduto++;
+
+            Label nomeprod = new Label();
+            nomeprod.Name = "txtnome";
+            nomeprod.Location = new Point(185);
+
+
+            panel1.Controls.Add(nomeprod);
+            
+        }
+    }
+}
